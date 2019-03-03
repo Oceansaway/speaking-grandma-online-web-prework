d5f0dd5b3d2a85444b5bb1f86e694573bb1adc0a
Stephanie Purple <spurple133@gmail.com>
2019-03-03 18:00:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ if phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
-elsif phrase != phrase.downcase
+else
   return"NO, NOT SINCE 1938."
 end
 end
